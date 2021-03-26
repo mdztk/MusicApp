733c4df655a5565e859e947c6c355fbf83ad3674
mdzt <max.detchik@gmail.com>
2021-03-26 17:17:47 +0300

    add app theme to activity

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MusicApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
